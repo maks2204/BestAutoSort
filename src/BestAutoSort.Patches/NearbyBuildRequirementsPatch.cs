95df34e8a91f6467827ed3b1730b1f99f80bfd45
Maxim <maks22004@gmail.com>
2026-09-14 01:41:00 +0300

    DIAG: log build-requirements postfix firing (fixed casts)

--- src/BestAutoSort.Patches/NearbyBuildRequirementsPatch.cs
+++ src/BestAutoSort.Patches/NearbyBuildRequirementsPatch.cs
@@ -15,11 +15,20 @@ internal static class NearbyBuildRequirementsPatch
 	private static void Postfix(Player __instance, Piece piece, RequirementMode mode, ref bool __result)
 	{
 		// DIAG-PLACE (temporary): prove the postfix fires.
-		Plugin.LogInstance.LogInfo((object)("[ChestTX] diag-have piece=" + (((Object)(object)piece != (Object)null && piece.m_name != null) ? piece.m_name : "?") + " mode=" + ((int)mode) + " vanilla=" + (__result ? "1" : "0")));
+		string diagName = "?";
+		try
+		{
+			if ((Object)(object)piece != (Object)null && piece.m_name != null)
+				diagName = piece.m_name;
+		}
+		catch
+		{
+		}
+		Plugin.LogInstance.LogInfo((object)("[ChestTX] diag-have piece=" + diagName + " mode=" + ((int)mode) + " vanilla=" + (__result ? "1" : "0")));
 		if (!__result && (int)mode == 0)
 		{
 			__result = NearbyResourceService.HasPieceRequirements(__instance, piece);
-			Plugin.LogInstance.LogInfo((object)"[ChestTX] diag-have ours=" + __result);
+			Plugin.LogInstance.LogInfo((object)("[ChestTX] diag-have ours=" + (__result ? "1" : "0")));
 		}
 		else if (!__result && (int)mode == 2)
 		{

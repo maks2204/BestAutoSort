813e7ccfe292d27aa992e1e460eb8492372a1c0b
Maxim <maks22004@gmail.com>
2026-09-14 01:40:03 +0300

    DIAG: log build-requirements postfix firing

--- src/BestAutoSort.Patches/NearbyBuildRequirementsPatch.cs
+++ src/BestAutoSort.Patches/NearbyBuildRequirementsPatch.cs
@@ -1,4 +1,5 @@
 using System;
+using BestAutoSort;
 using BestAutoSort.Runtime;
 using HarmonyLib;
 
@@ -13,9 +14,12 @@ internal static class NearbyBuildRequirementsPatch
 {
 	private static void Postfix(Player __instance, Piece piece, RequirementMode mode, ref bool __result)
 	{
+		// DIAG-PLACE (temporary): prove the postfix fires.
+		Plugin.LogInstance.LogInfo((object)("[ChestTX] diag-have piece=" + (((Object)(object)piece != (Object)null && piece.m_name != null) ? piece.m_name : "?") + " mode=" + ((int)mode) + " vanilla=" + (__result ? "1" : "0")));
 		if (!__result && (int)mode == 0)
 		{
 			__result = NearbyResourceService.HasPieceRequirements(__instance, piece);
+			Plugin.LogInstance.LogInfo((object)"[ChestTX] diag-have ours=" + __result);
 		}
 		else if (!__result && (int)mode == 2)
 		{
